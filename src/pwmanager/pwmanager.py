andbof/pwmanager

commit 37c39649b4b9d03c25576a820c3ea236cb46a142

Provide gpg_path config directive
On some platforms, the gpg binary can be configured to launch either
gpg1 or gpg2. The python3-gnupg only works with gpg2, and thus sometimes
this binary is called 'gpg' and sometimes 'gpg2'.

Provide a configuration directive so this can be changed on a
per-installation basis. If omitted, the default 'gpg' without an
absolute path is used.

src/pwmanager/pwmanager.py
@@ -50,14 +50,14 @@ def get_all_passwords(datapath):
     return accounts
 
 
-def get_all_pubkeys(fps, ldap, use_agent, gnupghome):
+def get_all_pubkeys(fps, ldap, use_agent, gpg_path, gnupghome):
     """
     Return dict {"email": ["public key in PEM format",...]}
     """
 
     r = {}
 
-    with GPG(use_agent, gnupghome) as gpg:
+    with GPG(use_agent, gpg_path, gnupghome) as gpg:
         for fp in fps:
             debug('Fetching public key with fingerprint {} (from configuration file)'.format(fp))
             key = gpg.find_key(fp)
@@ -129,7 +129,7 @@ def do_add(datapath, path, host, user, encpw, exist_ok):
             git.push_master()
 
 
-def _add_pw(host, user, password, datapath, keys, exist_ok, gnupghome):
+def _add_pw(host, user, password, datapath, keys, exist_ok, gpg_path, gnupghome):
     accounts = get_all_passwords(datapath)
 
     if not exist_ok and accounts.exists(host, user):
@@ -145,7 +145,7 @@ def _add_pw(host, user, password, datapath, keys, exist_ok, gnupghome):
 
     # We are only encrypting and using different keyrings, so do not use the
     # users gpg-agent even if we were instructed to do so.
-    with GPG(False, gnupghome) as gpg:
+    with GPG(False, gpg_path, gnupghome) as gpg:
         for email, fp_list in keys.items():
             debug('Encrypting to {} ({} key{})'.format(
                 email, len(fp_list), 's' if len(fp_list) > 1 else ''))
@@ -178,7 +178,7 @@ def add_pw(cfg, args, exist_ok=False):
             cfg['gnupg'].getboolean('use_agent'), cfg['gnupg']['home'])
 
     r = _add_pw(args.host, args.user, args.password, cfg['global']['datapath'],
-            keys, exist_ok, cfg['gnupg']['home'])
+            keys, exist_ok, cfg['gnupg']['gpg_path'], cfg['gnupg']['home'])
     print("Successfully encrypted password for {}/{} to {} recipient{}.".format(
         args.host, args.user, r, 's' if r > 1 else ''))
 
@@ -199,8 +199,8 @@ def _get_pwds(datapath, gpg, host, user):
     return r
 
 
-def get_pwds(host, user, datapath, use_agent, gnupghome, gnupgpass):
-    with GPG(use_agent, gnupghome) as gpg:
+def get_pwds(host, user, datapath, use_agent, gpg_path, gnupghome, gnupgpass):
+    with GPG(use_agent, gpg_path, gnupghome) as gpg:
         if not use_agent:
             # gpg-agent should automatically popup a different password dialog so
             # we should only ask for the password if we're not using it
@@ -238,7 +238,8 @@ def print_result(pwds, host, user):
 
 def get_pw(cfg, args):
     accs = get_pwds(args.host, args.user, cfg['global']['datapath'],
-            cfg['gnupg'].getboolean('use_agent'), cfg['gnupg']['home'], args.gnupgpass)
+            cfg['gnupg'].getboolean('use_agent'), cfg['gnupg']['gpg_path'],
+            cfg['gnupg']['home'], args.gnupgpass)
     print_result(accs, args.host, args.user)
 
 
@@ -254,8 +255,8 @@ def list_accs(cfg, args):
     print_result(accs, args.host, args.user)
 
 
-def get_unique_password(host, user, datapath, use_agent, gnupghome, gnupgpass):
-    pwds = get_pwds(host, user, datapath, use_agent, gnupghome, gnupgpass)
+def get_unique_password(host, user, datapath, use_agent, gpg_path, gnupghome, gnupgpass):
+    pwds = get_pwds(host, user, datapath, use_agent, gpg_path, gnupghome, gnupgpass)
     if not pwds:
         raise KeyError
     elif len(pwds) > 1:
@@ -267,7 +268,8 @@ def get_unique_password(host, user, datapath, use_agent, gnupghome, gnupgpass):
 def pipe_pw(cfg, args):
     try:
         pwd = get_unique_password(args.host, args.user, cfg['global']['datapath'],
-                cfg['gnupg'].getboolean('use_agent'), cfg['gnupg']['home'], args.gnupgpass)
+                cfg['gnupg'].getboolean('use_agent'), cfg['gnupg']['gpg_path'],
+                cfg['gnupg']['home'], args.gnupgpass)
     except KeyError:
         print("No matches for host '{}' {}".format(args.host,
             "and user '{}'".format(args.user) if args.user is not None else ''),
@@ -354,7 +356,7 @@ def sync_pws(cfg, args):
     keys = get_all_pubkeys(get_fps_from_conf(cfg), ldap,
             cfg['gnupg'].getboolean('use_agent'), cfg['gnupg']['home'])
 
-    with GPG(use_agent=False) as enc_gpg:
+    with GPG(gpg_path=cfg['gpg_path'], use_agent=False) as enc_gpg:
         for email, fps in keys.items():
             debug('Encrypting to {} ({} key{})'.format(
                 email, len(fps), 's' if len(keys) > 1 else ''))
@@ -362,6 +364,7 @@ def sync_pws(cfg, args):
                 enc_gpg.add_recipient(fp)
 
         with GPG(use_agent=cfg['gnupg'].getboolean('use_agent'),
+                gpg_path=cfg['gnupg']['gpg_path'],
                 gnupghome=cfg['gnupg']['home']) as dec_gpg:
             if not cfg['gnupg'].getboolean('use_agent'):
                 # gpg-agent should automatically popup a different password dialog so

src/pwmanager/config.py
@@ -27,6 +27,9 @@ def parse(path):
     # Some keys have default values if not specified
     if not 'keys' in c['global']:
         c['global']['keys'] = ''
+    if not 'gpg_path' in c['gnupg']:
+        # 'gpg' without absolute path is the same default as python3-gnupg uses
+        c['gnupg']['gpg_path'] = 'gpg'
 
     return c
 
@@ -49,6 +52,7 @@ def print_sample(path):
         #keys = fingerprint1,fingerprint2
 
         [gnupg]
+        #gpg_path = gpg
         home = {HOME}/.gnupg
         use_agent = yes
 

src/pwmanager/data/pwmanager.conf.sample
@@ -13,6 +13,7 @@ datapath = {CONFDIR}/data
 #keys = fingerprint1,fingerprint2
 
 [gnupg]
+#gpg_path = gpg
 home = {HOME}/.gnupg
 use_agent = yes
 

src/pwmanager/gpgwrap.py
@@ -20,8 +20,9 @@ class GPG():
 
     Decryption may use either the temporary directory or the real user keyring.
     """
-    def __init__(self, use_agent, gnupghome=None):
+    def __init__(self, use_agent, gpg_path, gnupghome=None):
         self.use_agent = use_agent
+        self.gpg_path = gpg_path
         self.gnupghome = gnupghome
 
     def __enter__(self):
@@ -32,7 +33,8 @@ class GPG():
         else:
             self.tempdir = None
 
-        self.gpg = gnupg.GPG(gnupghome=self.gnupghome, use_agent=self.use_agent)
+        self.gpg = gnupg.GPG(gpgbinary=self.gpg_path, gnupghome=self.gnupghome,
+                use_agent=self.use_agent)
         self.gpg.encoding = 'latin-1'
         self.encrypt_to = []
         self.passphrase = None

tests/test_gpgwrap.py
@@ -18,7 +18,7 @@ class TestGPG(unittest.TestCase):
         return enter
 
     def setUp(self):
-        self.gpg = TestGPG.setUp_with_cm(self, GPG(use_agent=False))
+        self.gpg = TestGPG.setUp_with_cm(self, GPG(gpg_path='gpg', use_agent=False))
         self.tempdir = tempfile.mkdtemp()
 
     def tearDown(self):

tests/test_manage.py
@@ -43,7 +43,7 @@ class TestManage(unittest.TestCase):
         self.old_get_all_pubkeys = pwmanager.get_all_pubkeys
         pwmanager.get_all_pubkeys = self.get_testkey
 
-        self.gpg = TestManage.setUp_with_cm(self, GPG(False, gnupghome=self.tempdir))
+        self.gpg = TestManage.setUp_with_cm(self, GPG(False, gpg_path='gpg', gnupghome=self.tempdir))
         self.gpg.add_recipient(testkey['private'])
         self.gpg.set_passphrase(testkey['passphrase'])
         self.run_proc(["/usr/bin/git", "init", "--bare", self.repodir], '/')
@@ -67,6 +67,7 @@ class TestManage(unittest.TestCase):
                 'keys': '',
         }
         self.def_config['gnupg'] = {
+                'gpg_path': 'gpg',
                 'home': self.tempdir,
                 'use_agent': False,
         }
@@ -147,13 +148,13 @@ class TestManage(unittest.TestCase):
             self.ensure_acc_exists()
         self.ensure_acc_exists('other')
 
-
     def test_pipe(self):
         self.ensure_acc_not_exists()
         with self.assertRaises(KeyError):
             pwmanager.get_unique_password(self.def_args.host,
                 None, self.def_config['global']['datapath'],
                 self.def_config['gnupg'].getboolean('use_agent'),
+                self.def_config['gnupg']['gpg_path'],
                 self.def_config['gnupg']['home'], self.def_args.gnupgpass
             )
 
@@ -162,6 +163,7 @@ class TestManage(unittest.TestCase):
         self.assertEqual(pwmanager.get_unique_password(self.def_args.host,
                 self.def_args.user, self.def_config['global']['datapath'],
                 self.def_config['gnupg'].getboolean('use_agent'),
+                self.def_config['gnupg']['gpg_path'],
                 self.def_config['gnupg']['home'], self.def_args.gnupgpass
             ), self.def_args.password)
 
@@ -172,8 +174,23 @@ class TestManage(unittest.TestCase):
             pwmanager.get_unique_password(self.def_args.host,
                 None, self.def_config['global']['datapath'],
                 self.def_config['gnupg'].getboolean('use_agent'),
+                self.def_config['gnupg']['gpg_path'],
                 self.def_config['gnupg']['home'], self.def_args.gnupgpass
             )
+
+    def test_gpg_path(self):
+        pwmanager.add_pw(self.def_config, self.def_args)
+        self.ensure_acc_exists()
+        with self.assertRaises(ValueError) as e:
+            pwmanager.get_unique_password(self.def_args.host,
+                None, self.def_config['global']['datapath'],
+                self.def_config['gnupg'].getboolean('use_agent'),
+                '/bin/false', self.def_config['gnupg']['home'],
+                self.def_args.gnupgpass
+            )
+        # We need to ensure the ValueError was actually because /bin/false
+        # returned exit code 1. Let's hope this syntax never changes.
+        self.assertTrue(str(e.exception) == 'Error invoking gpg: 1: ')
 
 
 if __name__ == '__main__':

commit 2a047ddb364d37bb68152f422cb82910878e451d

pwmanager: fix bug when adding or syncing passwords
Commit 37c39649 ("Provide gpg_path config directive") introduced a new
gpg parameter but unfortunately did not update all function calls
correctly. Fix it.

src/pwmanager/pwmanager.py
@@ -175,7 +175,8 @@ def add_pw(cfg, args, exist_ok=False):
         ldap = cfg['ldap']
 
     keys = get_all_pubkeys(get_fps_from_conf(cfg), ldap,
-            cfg['gnupg'].getboolean('use_agent'), cfg['gnupg']['home'])
+            cfg['gnupg'].getboolean('use_agent'), cfg['gnupg']['gpg_path'],
+            cfg['gnupg']['home'])
 
     r = _add_pw(args.host, args.user, args.password, cfg['global']['datapath'],
             keys, exist_ok, cfg['gnupg']['gpg_path'], cfg['gnupg']['home'])
@@ -354,9 +355,10 @@ def sync_pws(cfg, args):
         ldap = cfg['ldap']
 
     keys = get_all_pubkeys(get_fps_from_conf(cfg), ldap,
-            cfg['gnupg'].getboolean('use_agent'), cfg['gnupg']['home'])
+            cfg['gnupg'].getboolean('use_agent'), cfg['gnupg']['gpg_path'],
+            cfg['gnupg']['home'])
 
-    with GPG(gpg_path=cfg['gpg_path'], use_agent=False) as enc_gpg:
+    with GPG(gpg_path=cfg['gnupg']['gpg_path'], use_agent=False) as enc_gpg:
         for email, fps in keys.items():
             debug('Encrypting to {} ({} key{})'.format(
                 email, len(fps), 's' if len(keys) > 1 else ''))
@@ -512,7 +514,7 @@ actions = {
             'opt_args': {},
         },
         'sync': {
-            'help': 'Go through all passwords and reencrypt to all configured public key (and noone else)',
+            'help': 'Go through all passwords and reencrypt to all configured public keys (and noone else)',
             'method': sync_pws,
             'pos_args': [],
             'opt_args': {